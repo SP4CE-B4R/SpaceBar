f57b6fbf4b72be64561861b4f6a3d809ffddde6d
Volty <pranshuj73@gmail.com>
2020-08-11 21:00:22 +0530

    added Post model

--- spacebar/website/models.py
+++ spacebar/website/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=64, help_text="Enter the title of your post")
+    body = models.CharField(max_length=500, help_text="Enter the body of your post")
+    date = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
