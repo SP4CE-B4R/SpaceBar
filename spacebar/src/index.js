8e73f5360f8000be8d4214df357f1be3b630ae23
volt9801 <pranshuj73@gmail.com>
2020-12-30 19:44:01 +0530

    🛠     set up tailwindcss

--- spacebar/src/index.js
+++ spacebar/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// css imports
+import './index.css';
+import './tailwind.output.css';
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ spacebar/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+    'src/**/*.js',
+    'src/**/*.jsx',
+    'src/**/*.ts',
+    'src/**/*.tsx',
+    'public/**/*.html',
+  ],
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
