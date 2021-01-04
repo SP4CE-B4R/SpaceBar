9c0a5b414073c402d9f3a8b9e66b03e1064e6596
volt9801 <pranshuj73@gmail.com>
2021-01-05 01:00:49 +0530

    ➕ Added django backend to store posts

---
+++ backend/api/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    body = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} written by {self.author} on {self.date}"
