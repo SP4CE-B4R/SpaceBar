be21304fe19b7f468b3752a42d900c8071e8da1e
pranshuj73 <58560660+pranshuj73@users.noreply.github.com>
2021-05-16 22:19:04 +0530

    added storybook js

---
+++ .storybook/preview.js
@@ -0,0 +1,16 @@
+// .storybook/preview.js
+
+import { themes } from '@storybook/theming';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  docs: {
+    theme: themes.dark,
+  },
+}
